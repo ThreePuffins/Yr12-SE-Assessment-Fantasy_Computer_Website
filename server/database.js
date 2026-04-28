34b31338efe2b2d23d523e4d42ca8667401595b3
ThreePuffins <Tristan.Randall4@education.nsw.gov.au>
2026-04-28 20:07:42 +1000

    Create signup page, user and game templates and 404 page

--- server/database.js
+++ server/database.js
@@ -2,25 +2,52 @@ const sqlite = require('sqlite3')
 const db = new sqlite.Database(":memory:")
 
 db.serialize(() => {
-    const query = 'CREATE TABLE games (id int, name string, release_day int, release_month int, release_year int)';
-    db.run(query);
-    const stmt = db.prepare('INSERT INTO games (id, name, release_day, release_month, release_year) VALUES (?, ?, ?, ?, ?)');
+    const init_gay = 'CREATE TABLE IF NOT EXISTS games (id int, name string, release_day int, release_month int, release_year int)';
+    db.run(init_gay);
+    const init_ceridwen = 'CREATE TABLE IF NOT EXISTS users (id int, username string, email string, password string)';
+    db.run(init_ceridwen);
 
+
+    const fill_user = db.prepare('INSERT INTO games (id, name, release_day, release_month, release_year) VALUES (?, ?, ?, ?, ?)');
+    
     for (let i = 0; i < 10; i++) {
-        stmt.run([i, `Ipsum${i}`, i, i, i]);
+        fill_user.run([i, `Ipsum${i}`, i, i, i]);
     }
 
-    stmt.finalize();
+    const fill_gay = db.prepare('INSERT INTO users (id, username, password) VALUES (?, ?, ?)');
+    
+    for (let i = 0; i < 17; i++) {
+        fill_gay.run([i, `Ceridwen${i}`, "mq02v82bkxh2b73v"]);
+    }
+
+    fill_gay.finalize();
+    fill_user.finalize();
 })
 
-
-function get(cb) {
-    db.all("SELECT * FROM games", (err, rows) => {
+function get_game(id, cb) {
+    const stmt = db.prepare("SELECT * FROM games WHERE id = (?)");
+    stmt.all(id, (err, rows) => {
         if (err) {
             cb(err, null);
             return;
         }
-        cb(null, rows);
-    });
+        cb(null, rows[0]);
+    })
 }
-module.exports = { get };
+
+function get_user(id, cb) {
+    const stmt = db.prepare("SELECT * FROM users WHERE id = (?)");
+    stmt.all(id, (err, rows) => {
+        if (err) {
+            cb(err, null);
+            return;
+        }
+        cb(null, rows[0]);
+    })
+}
+
+function create_user(id, username, email, password) {
+    const stmt = db.prepare("INSERT INTO users (id, username, email, password) VALUES (?, ?, ?, ?)");
+    stmt.all([id, username, email, password])
+}
+module.exports = { get_game, get_user};
--- server/main.js
+++ server/main.js
@@ -13,34 +13,67 @@ app.use(
         origin: ["http://localhost:8080"],
     })
 )
-
-app.use(express.static('client'))
+app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
-
 app.set('views', './views');
 
+app.get('/signup', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public', 'signup.html'));
+})
 
-app.get('/g/:g', (req, res) => {
-  const data = {
-    title: req.params.g
-  };
+app.get('/u/:id', (req, res) => {
+  const id = req.params.id
 
-
-  database.get((err, rows) => {
-      if (err) {console.log(err);}
-      else { console.log(rows) }
+  database.get_user(id, (err, rows) => {
+    if (err) { console.log(err); }
+    else if (rows) {
+      const data = {
+        username: rows.username,
+        password: rows.password
+      }
+      if (data.username) res.render('user', data);
+    }
+    else res.render('fuck');
   })
-
-  res.render('index', data);
 });
 
+app.get('/g/:id', (req, res) => {
+  
+  const id = req.params.id
+
+  database.get_game(id, (err, rows) => {
+    if (err) { console.log(err); }
+    else if (rows) {
+      const data = {
+        name: rows.name
+      }
+      if (data.name) res.render('game', data);
+    }
+    else res.render('fuck');
+  })
+});
 
 app.get("/api/hello", (req, res) => {
   res.json({message: "hello beijing wasgud"});
 });
 
+app.use(function(req, res) {
+  res.status(404);
+
+  if (req.accepts('html')) {
+    res.render('404');
+    return;
+  }
+
+  if (req.accepts('json')) {
+    res.json({ error: 'Not found' });
+    return;
+  }
+  
+  res.type('txt').send('Not found');
+});
+
 app.listen(PORT, () => {
     console.log(`hee-ho check out http://localhost:${PORT}`);
-    //res.json(database.get()[0].name);
 });
